afa58011be3bbc6f1298ec534d4a811f437a00e7
Oleg Ovramets <olegovramets@gmail.com>
2022-07-30 21:02:48 +0300

    update homepage

--- components/AuthComponents/RegisterForm/TypeChoosingStage/TypeChoosingStage.tsx
+++ components/AuthComponents/RegisterForm/TypeChoosingStage/TypeChoosingStage.tsx
@@ -15,14 +15,14 @@ const TypeChoosingStage: FC<TypeChoosingStageProps> = ({ accountType, setAccount
             className={accountType === "employers" ? classNames(styles.typeVariant, styles.activeVariant) : styles.typeVariant}
             onClick={() => setAccountType("employers")}
           >
-            <h3>I&aposm Employer</h3>
+            <h3>I am Employer</h3>
             <p>and I want to find the developer</p>
           </div>
           <div
             className={accountType === "developers" ? classNames(styles.typeVariant, styles.activeVariant) : styles.typeVariant}
             onClick={() => setAccountType("developers")}
           >
-            <h3>I&aposm Developer</h3>
+            <h3>I am Developer</h3>
             <p>and I want to find the work</p>
           </div>
           <button type="button" onClick={goToNextStep} className={styles.registerBtn} style={accountType === "" ? {opacity: 0.5} : {opacity: 1}}>Next</button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,20 @@ import { useDispatch } from "react-redux";
 import { GiTrophiesShelf } from "@react-icons/all-files/gi/GiTrophiesShelf";
 import { FaEthereum } from "@react-icons/all-files/fa/FaEthereum";
 import { BsPencil } from "@react-icons/all-files/bs/BsPencil"
+import classNames from "classnames";
 
 const Home: FC = () => {
-  // const { isAuthModalOpened } = useAppSelector(
-  //   (state) => state.authReducer
-  // );
   const dispatch = useDispatch();
   return (
     <>
       <div className={styles.headingSection}>
         <h1>Want to find a work or a worker?</h1>
-        <h3>GoFreelance is a first blockchain based freelance exchange</h3>
+        <h3>GoFreelance is a first blockchain-based freelance exchange</h3>
         <button
           className={styles.authBtn}
           onClick={() => dispatch(openAuthModal())}
         >
-          Let&quots start now
+          Start now
         </button>
       </div>
       <div className={styles.mainSection}>
@@ -31,7 +29,7 @@ const Home: FC = () => {
             <div className={styles.cards}>
               <div className={styles.card}>
                 <h3>1. Safe payment system</h3>
-                <FaEthereum className={styles.cardIcon}/>
+                <FaEthereum color="#471296" className={classNames(styles.cardIcon, styles.ethIcon)}/>
                 <p>
                   We provide the safest payment system because it&quots launched on
                   Ethereum network
@@ -39,14 +37,14 @@ const Home: FC = () => {
               </div>
               <div className={styles.card}>
                 <h3>2. Comfortable design</h3>
-                <BsPencil className={styles.cardIcon}/>
+                <BsPencil color="#471296" className={styles.cardIcon}/>
                 <p>
                   Peoples loves our simple and easy to understand design
                 </p>
               </div>
               <div className={styles.card}>
                 <h3>3. We want to become better</h3>
-                <GiTrophiesShelf className={styles.cardIcon}/>
+                <GiTrophiesShelf color="#471296" className={styles.cardIcon}/>
                 <p>
                   We really love our community and listens to their proposals
                 </p>
